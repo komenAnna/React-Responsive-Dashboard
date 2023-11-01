9351d6cec866fa274a4c04cec5c33f9f1de6452f
komenAnna <komenriana@gmail.com>
2023-11-01 09:03:01 +0300

    [update]:finished the updates component

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -8,6 +8,12 @@ import {
     UilUsdSquare,
     UilMoneyWithdrawal,
 } from '@iconscout/react-unicons';
+
+//Updates imgs
+import img1 from '../imgs/img1.png'
+import img2 from '../imgs/img2.png'
+import img3 from '../imgs/img3.png'
+
 export const SidebarData = [
     {
         icon: UilEstate,
@@ -85,4 +91,26 @@ export const CardsData = [
           },
         ],
       },
+]
+
+// Updates data
+export const UpdatesData = [
+  {
+    img: img1,
+    name: "Andrew Thomas",
+    noti: " has ordered Apple smart watch 2500mh battery.",
+    time: "25 seconds ago",
+  },
+  {
+    img: img2,
+    name: "James Bond",
+    noti: " has received Samsung gadget for charging battery.",
+    time: "30 minutes ago",
+  },
+  {
+    img: img3,
+    name: "Iron Man",
+    noti: " has ordered Apple smart watch, samsung Gear 2500mh battery.",
+    time: "2 hours ago",
+  },
 ]
--- src/components/Updates/Updates.jsx
+++ src/components/Updates/Updates.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Updates.css'
+import { UpdatesData} from '../../Data/Data'
 
 const Updates = () => {
   return (
-    <div className='Updates'>Updates</div>
+    <div className='Updates'>
+      {UpdatesData.map((update)=>{
+        return(
+          <div className='update'>
+            <img src={update.img} alt="customer-profile" />
+            <div className="notification">
+              <div style={{marginBottom: '0.5rem'}}>
+                <span>{update.name}</span>
+                <span>{update.noti}</span>
+              </div>
+              <span>{update.time}</span>
+            </div>
+            
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
